40c89d09af47f83681dec185e54fbddc333b86c4
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-05-30 16:52:11 +0100

    feat: add Header Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
-import Link from "next/link";
+import { Header } from '@/components/header';
+import { Geist, Geist_Mono } from 'next/font/google';
 
-export default function HomePage() {
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export default function Home() {
   return (
-    <div>
-     <h2>Home</h2>
-
-     <Link href='/users'>Users Page</Link>
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
+      <Header />
     </div>
   );
 }
