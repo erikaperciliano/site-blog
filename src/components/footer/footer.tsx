eb47901a291bcc888af0b224fe8acc0a89a2d915
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-06-07 20:22:43 +0100

    feat: add style guide

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,22 +3,22 @@ import { Logo } from '../logo';
 
 export const Footer = () => {
   return (
-    <footer className="border-t border-white/10 bg-dark">
+    <footer className="bg-gray-500">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between md:flex-row gap-8 py-8">
           <Logo />
 
-          <nav className="flex  md:flex-row items-center gap-4 text-sm text-muted-foreground">
-            <Link href="/terms-of-use" className="hover:text-primary">
+          <nav className="flex  md:flex-row items-center gap-4 text-sm text-blue-100">
+            <Link href="/terms-of-use" className="hover:text-blue-200">
               Terms of Use
             </Link>
             <Link
               href="/privacy-policy"
-              className="hover:text-primary"
+              className="hover:text-blue-200"
             >
               Privacy Policy
             </Link>
-            <Link href="/feedback" className="hover:text-primary">
+            <Link href="/feedback" className="hover:text-blue-200">
               Feedback
             </Link>
           </nav>
