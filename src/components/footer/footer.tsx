6a6da1ba4dea02e6539bfb90986d1cbd45b0c51a
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-06-07 19:51:33 +0100

    feat: add some adjustments and add Footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const Footer = () => {
+  return (
+    <footer className="border-t border-white/10 bg-dark">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between md:flex-row gap-8 py-8">
+          <Link href="/">
+            <Image src="/logo.svg" alt="Logo site" width={116} height={32} />
+          </Link>
+
+          <nav className="flex  md:flex-row items-center gap-4 text-sm text-muted-foreground">
+            <Link href="/terms-of-use" className="hover:text-primary">
+              Terms of Use
+            </Link>
+            <Link
+              href="/privacy-policy"
+              className="hover:text-primary"
+            >
+              Privacy Policy
+            </Link>
+            <Link href="/feedback" className="hover:text-primary">
+              Feedback
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
+};
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,24 +1,26 @@
-import Link from "next/link"
-import { Button } from "../ui/button"
-import { ActiveLink } from "../active-link";
+import Image from 'next/image';
+import Link from 'next/link';
+import { ActiveLink } from '../active-link';
+import { Button } from '../ui/button';
 
 export const Header = () => {
-    return (
-        <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60">
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-80">
-                <div className="flex h-16 items-center justify-between">
-                    <Link href='/'>Logo</Link>
-                    
-                    <nav className="flex items-center gap-6">
-                        <ActiveLink href='/'>Home</ActiveLink>
-                        <ActiveLink href='/blog'>Blog</ActiveLink>
+  return (
+    <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 ls:px-80">
+        <div className="flex h-16 items-center justify-between">
+          <Link href="/">
+            <Image src="/logo.svg" alt="Logo site" width={116} height={32} />
+          </Link>
 
-                        <Button variant={"secondary"} asChild>
-                            <Link href='/start'>Start</Link>
-                        </Button>
-                    </nav>
-                </div>
-            </div>
-        </header>
-    )
-}
+          <nav className="flex items-center gap-6">
+            <ActiveLink href="/">Home</ActiveLink>
+            <ActiveLink href="/blog">Blog</ActiveLink>
+            <Button variant="secondary" asChild>
+              <Link href="/start">Start</Link>
+            </Button>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
