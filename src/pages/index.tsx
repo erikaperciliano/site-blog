c7c0614b65bb81649f3433d56f8a1fb36476d713
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-06-09 21:59:54 +0100

    feat: add Support Section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { FeatureSection } from "@/components/feature-section";
 import { HeroSection } from "@/components/hero-section";
+import { SuportSection } from "@/components/support-section";
 
 export default function Home() {
   return (
     <>
       <article className="flex flex-col">
-        <HeroSection/>
-        <FeatureSection  />
+        <HeroSection />
+        <FeatureSection />
+        <SuportSection />
       </article>
     </>
   );
