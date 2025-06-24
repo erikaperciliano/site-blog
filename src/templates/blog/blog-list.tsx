229a35b5757da1469c41d5cf8e76bd8fb8bd45ef
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-06-24 18:30:19 -0300

    feat: adding dynamic information to PostCard

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,26 +1,46 @@
-import { Search } from "@/components/search";
-import { useRouter } from "next/router";
-import { PostCard } from "./components/post-card";
+import { Search } from '@/components/search';
+import { useRouter } from 'next/router';
+import { PostCard } from './components/post-card';
 
 export function BlogList() {
-    const router = useRouter()
-    const query = router.query.q as string
-    const pageTitle = query ? `Search results for "${query}"` : 'Tips and strategies to boost your business'
+  const router = useRouter();
+  const query = router.query.q as string;
+  const pageTitle = query
+    ? `Search results for "${query}"`
+    : 'Tips and strategies to boost your business';
 
-    return(
-        <div className="flex flex-col py-24 flex-grow h-full">
-            <header className="">
-                <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
-                    <div  className="flex flex-col gap-4 md:px-0">
-                        <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">BLOG</span>
-                    
-                        <h1 className="text-balance text-start md:text-left text-heading-lg md:text-heading-xl max-w-2xl text-gray-100">{pageTitle}</h1>
-                    </div>
-                    <Search />
-                </div>
-            </header>
+  return (
+    <div className="flex flex-col py-24 flex-grow h-full">
+      <header className="">
+        <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
+          <div className="flex flex-col gap-4 md:px-0">
+            {/* TAG */}
+            <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">
+              BLOG
+            </span>
 
-            <PostCard />
+            {/* Titulo */}
+            <h1 className="text-balance text-start md:text-left text-heading-lg md:text-heading-xl max-w-2xl text-gray-100">
+              {pageTitle}
+            </h1>
+          </div>
+          {/* Search */}
+          <Search />
         </div>
-    )
+      </header>
+
+      {/* Listagem de posts */}
+      <PostCard
+        title="Turning your business into an online store"
+        description="If you are looking for a simple and effective way to sell your products online..."
+        date="20/12/2024"
+        slug="/transforming"
+        image="/assets/first-post.png"
+        author={{
+          avatar: '/customer-01.png',
+          name: 'Aspen Dokidis',
+        }}
+      />
+    </div>
+  );
 }
--- src/templates/blog/components/post-card/post-card.tsx
+++ src/templates/blog/components/post-card/post-card.tsx
@@ -1,17 +1,34 @@
 import Image from "next/image"
 import Link from "next/link"
 
-export const PostCard = () => {
+type Author = {
+    name: string;
+    avatar: string;
+}
+
+type PostCardProps = {
+    slug: string;
+    title: string;
+    description: string;
+    image: string;
+    date: string;
+    author: Author;
+}
+
+export const PostCard = ({ slug, title, description, image, date, author }: PostCardProps) => {
     return(
-        <Link className="w-full max-w-2xl rounded-3xl border-[1px] border-gray-400 bg-gray-600 overflow-hidden transition-all duration-300 hover:border-[1px] hover:border-blue-300" href={`/blog/`}>
+        <Link 
+            className="w-full max-w-2xl rounded-[12px] border-[1px] border-gray-400 bg-gray-600 overflow-hidden transition-all duration-300 hover:border-[1px] hover:border-blue-300" 
+            href={`/blog/${slug}`}
+        >
             <div className="p-2 rounded-md overflow-hidden">
                 <div className="relative">
-                    <div className="absolute top-0 right-0 px-3 py-1">
-                        <span className="text-body-xs text-gray-300">20/12/2022</span>
+                    <div className="absolute top-0 right-0 px-3 py-1 bg-gray-600 backdrop-blur-sm rounded-bl-[10px]">
+                        <span className="text-body-xs text-gray-300">{date}</span>
                     </div>
                     <Image
-                        className="w-full h-40 object-cover object-center"
-                        src={`/assets/first-post.png`}
+                        className="w-full h-40 object-cover object-center rounded-t-[8px]"
+                        src={image}
                         alt=""
                         width={288}
                         height={144}    
@@ -19,20 +36,20 @@ export const PostCard = () => {
                 </div>
 
                 <div className="px-2 mt-4 space-y-4">
-                    <h2 className="text-heading-sm text-gray-100 line-clamp-3">Turning your business into an online store</h2>
-                    <p className="text-gray-300 text-body-sm line-clamp-3">If you are looking for a simple and effective way to sell your products online, the Site...</p>
+                    <h2 className="text-heading-sm text-gray-100 line-clamp-3">{title}</h2>
+                    <p className="text-gray-300 text-body-sm line-clamp-3">{description}</p>
 
                     <div className="flex items-center gap-3 border-t border-gray-400 py-4">
                         <div className="relative h-5 w-5 md:h-6 md:w-6 overflow-hidden rounded-full border-blue-200 border-[1px]">
                             <Image
                                 className="object-cover rounded-md"
-                                src={'/customer-01.png'}
+                                src={author?.avatar}
                                 alt=""
                                 fill
                             />
                         </div>
 
-                        <span className="text-body-sm text-gray-300">Aspen Dokidis</span>
+                        <span className="text-body-sm text-gray-300">{author?.name}</span>
                     </div>
                 </div>
             </div>
